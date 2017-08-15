18040bc7224a633ca52548494d8e54f6519c4e67
Akshay Sonvane <akshay.sonvane93@gmail.com>
2017-08-15 09:57:25 -0700

    Added Util class.

---
+++ marklogic-rdf4j/src/main/java/com/marklogic/semantics/rdf4j/utils/Util.java
@@ -0,0 +1,38 @@
+package com.marklogic.semantics.rdf4j.utils;
+
+import org.eclipse.rdf4j.rio.RDFFormat;
+
+public class Util {
+    private static Util util = null;
+    private Util(){
+
+    }
+
+    public static Util getInstance()
+    {
+        if(util == null){
+            util = new Util();
+        }
+        return util;
+    }
+
+    /**
+     * public utility method that tests if an object is null
+     * @param item the item to check for null.
+     * @return boolean
+     */
+    public static Boolean notNull(Object item) {
+        return item!=null;
+    }
+
+    /**
+     * public utility method to check if the RDF format is supported by MarkLogic database.
+     * @param dataFormat the RDF format to check if supported by MarkLogic
+     * @return Boolean
+     */
+    public Boolean isFormatSupported(RDFFormat dataFormat){
+        return dataFormat.equals(RDFFormat.TURTLE) || dataFormat.equals(RDFFormat.RDFXML) || dataFormat.equals(RDFFormat.TRIG)
+            || dataFormat.equals(RDFFormat.NQUADS) || dataFormat.equals(RDFFormat.NTRIPLES) || dataFormat.equals(RDFFormat.RDFJSON)
+                || dataFormat.equals(RDFFormat.N3);
+    }
+}
