9ec5f157520904241a45863037c4afd2510c0a5d
Akshay Sonvane <akshay.sonvane93@gmail.com>
2017-09-15 11:36:57 -0700

    Bumped MarkLogic java client api to version 4.0.2

--- marklogic-rdf4j/src/test/java/com/marklogic/semantics/rdf4j/SPARQLRepositoryTest.java
+++ marklogic-rdf4j/src/test/java/com/marklogic/semantics/rdf4j/SPARQLRepositoryTest.java
@@ -64,7 +64,8 @@ public class SPARQLRepositoryTest extends Rdf4jTestBase {
         if(conn.isOpen()){conn.clear();}
         conn.close();
         conn = null;
-        rep.shutDown();
+        // TODO: Un-comment line when issue 811 gets resolved. https://github.com/marklogic/java-client-api/issues/811
+        //rep.shutDown();
         rep = null;
         logger.info("tearDown complete.");
     }
